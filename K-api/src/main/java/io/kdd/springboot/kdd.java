0c508c6dc838c058b7aaaac39cfeb966db44d33c
keldendraduldorji <keldendraduldorji@keldendraduldorji-VirtualBox>
2018-02-09 22:08:57 +0530

    First Commit

---
+++ K-api/src/main/java/io/kdd/springboot/kdd.java
@@ -0,0 +1,15 @@
+package io.kdd.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class kdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(kdd.class, args);
+	}
+
+}
